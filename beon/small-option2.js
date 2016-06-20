fd2d20e4412360c370c9ac0842a51d4f5d012149
Polar MediaVoice <polar@polarmobile.com>
2016-06-20 18:23:34 -0400

    Add files via upload

--- beon/small-option2.js
+++ beon/small-option2.js
@@ -17,7 +17,6 @@
         label: "Home",
         unit: {"server":"dfp","id":"/7814368/NativeAdUnit","size":"2x2","targets":{"category":"news","placement":"top"}},
         location: ".right_rail_flex",
-        append:true,
         infoText: "",
         infoButtonText: "",
         template: compiledTemplate0,
@@ -25,59 +24,7 @@
         onFill: function(data) { },
         onError: function(error) { }
     }]);
-    q().push(["injectCSS",["",
-".s-polar-outer {",
-"    border: 1px solid #2e7061;",
-"    border-top: 8px solid #2e7061;",
-"    padding: 10px;",
-"    width: 300px;",
-"    height: 250px;",
-"    margin-bottom: 20px;",
-"}",
-".s-polar-title {",
-"    font-family: ProximaNovaCond-Extrabld, NotoKufiArabic-Bold, Helvetica Neue, Helvetica, Roboto, Arial, sans-serif;",
-"    font-weight: 400;",
-"    text-transform: uppercase;",
-"    text-align: justify;",
-"    font-family: ProximaNovaCond-Extrabld, NotoKufiArabic-Bold, Helvetica Neue, Helvetica, Roboto, Arial, sans-serif;",
-"    color: black;",
-"    text-decoration: none;",
-"}",
-".s-polar-summary {",
-"    font-family: NotoNashkArabic, Helvetica Neue, Helvetica, Roboto, Arial, sans-serif;",
-"    line-height: 27px;",
-"    padding-top: 20px;",
-"    text-decoration: none;",
-"    color: #757575;",
-"}",
-".s-polar-video {",
-"    margin-top: 0px;",
-"    margin-bottom: 0px;",
-"}",
-".s-polar-title {",
-"    font-family: oswaldmedium;",
-"    font-weight: 400;",
-"    text-transform: uppercase;",
-"    text-align: center;",
-"}",
-".s-polar-sponsorbadge {",
-"    display: block;",
-"    margin-top: -5px;",
-"}",
-".s-polar-sponsorlabel {",
-"    float: left;",
-"    color: #757575;",
-"    font-size: small;",
-"    margin-top: 17px;",
-"}",
-".s-polar-sponsorlogo {",
-"    margin-top: 10px;",
-"    height: 20px;",
-"    width: auto;",
-"    float: right;",
-"    display: block;",
-"}",
-""].join("\n"), "head"]);
+    q().push(["injectCSS", ".polar-outer { border:1px solid #2e7061; border-top:8px solid #2e7061; padding:10px; width:300px; height:250px; margin-bottom: 20px; } .polar-title { font-family: 'ProximaNovaCond-Extrabld', 'NotoKufiArabic-Bold', 'Helvetica Neue', 'Helvetica', Roboto, Arial, sans-serif; font-weight: 400; text-transform: uppercase; text-align: justify; font-family: 'ProximaNovaCond-Extrabld', 'NotoKufiArabic-Bold', 'Helvetica Neue', 'Helvetica', Roboto, Arial, sans-serif; color: black; text-decoration: none; } .polar-summary { font-family: \"NotoNashkArabic\", \"Helvetica Neue\", \"Helvetica\", Roboto, Arial, sans-serif; line-height: 27px; padding-top: 20px; text-decoration: none; color: #757575;} .polar-video { margin-top: 0px; margin-bottom: 0px; } .polar-title { font-family: oswaldmedium; font-weight: 400; text-transform: uppercase; text-align: center; } .polar-sponsorbadge { display: block; margin-top:-5px; } .polar-sponsorlabel { float:left; color: #757575; font-size: small; margin-top:17px; } .polar-sponsorlogo { margin-top: 10px; height: 20px; width:auto; float:right; display: block; }", "head"]);
 
     q().push(["configureSecondaryPage", {
         binding: {
@@ -104,33 +51,34 @@
     }]);
 
     function templates () {
-/*
 
-   This function represents a pre-compiled Handlebars template. Pre-compiled
-   templates are not pretty, but they provide a very significant performance
-   boost, especially on mobile devices. For more information, see
-   http://handlebarsjs.com/precompilation.html.
+        /*
 
-   Note that this code has been generated from the following markup:
+          This function represents a pre-compiled Handlebars template. Pre-compiled
+          templates are not pretty, but they provide a very significant performance
+          boost, especially on mobile devices. For more information, see
+          http://handlebarsjs.com/precompilation.html.
 
-<div class="s-polar-outer"><a href="http://on.aol.com/video/spoilers-game-of-thrones-season-six-finale-will-blow-your-mind-5758b79be4b075144bea4691" rel="nofollow">
+          Note that this code has been generated from the following markup:
+
+<div class="polar-outer"><a href="http://on.aol.com/video/spoilers-game-of-thrones-season-six-finale-will-blow-your-mind-5758b79be4b075144bea4691" rel="nofollow">
       </a><div class="follow-us__header">
 <h3>The Mindi Project on Hulu</h3>
 </div>
 
-  <div class="s-polar-video">
+  <div class="polar-video">
       <iframe width="275"src="https://www.youtube.com/embed/_uwanee-lS4" frameborder="0" allowfullscreen></iframe>
   </div>
-  <div class="s-polar-sponsorbadge"> 
-    <span class="s-polar-sponsorlabel">PROMOTED</span>
-    <img class="s-polar-sponsorlogo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Hulu_logo.svg/2000px-Hulu_logo.svg.png" height="30px" alt="Hulu">
+  <div class="polar-sponsorbadge"> 
+    <span class="polar-sponsorlabel">PROMOTED</span>
+    <img class="polar-sponsorlogo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Hulu_logo.svg/2000px-Hulu_logo.svg.png" height="30px" alt="Hulu">
     
   </div>
   </div>
 
-*/
+        */
 
-  compiledTemplate0 = function (Handlebars,depth0,helpers,partials,data) {  this.compilerInfo = [4,'>= 1.0.0'];helpers = this.merge(helpers, Handlebars.helpers); data = data || {};    return "<div class=\"s-polar-outer\"><a href=\"http://on.aol.com/video/spoilers-game-of-thrones-season-six-finale-will-blow-your-mind-5758b79be4b075144bea4691\" rel=\"nofollow\">\n      </a><div class=\"follow-us__header\">\n<h3>The Mindi Project on Hulu</h3>\n</div>\n\n  <div class=\"s-polar-video\">\n      <iframe width=\"275\"src=\"https://www.youtube.com/embed/_uwanee-lS4\" frameborder=\"0\" allowfullscreen></iframe>\n  </div>\n  <div class=\"s-polar-sponsorbadge\"> \n    <span class=\"s-polar-sponsorlabel\">PROMOTED</span>\n    <img class=\"s-polar-sponsorlogo\" src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Hulu_logo.svg/2000px-Hulu_logo.svg.png\" height=\"30px\" alt=\"Hulu\">\n    \n  </div>\n  </div>";  };
+        compiledTemplate0 = function (Handlebars,depth0,helpers,partials,data) {  this.compilerInfo = [4,'>= 1.0.0'];helpers = this.merge(helpers, Handlebars.helpers); data = data || {};    return "<div class=\"polar-outer\"><a href=\"http://on.aol.com/video/spoilers-game-of-thrones-season-six-finale-will-blow-your-mind-5758b79be4b075144bea4691\" rel=\"nofollow\">\n      </a><div class=\"follow-us__header\">\n<h3>The Mindi Project on Hulu</h3>\n</div>\n\n  <div class=\"polar-video\">\n      <iframe width=\"275\"src=\"https://www.youtube.com/embed/_uwanee-lS4\" frameborder=\"0\" allowfullscreen></iframe>\n  </div>\n  <div class=\"polar-sponsorbadge\"> \n    <span class=\"polar-sponsorlabel\">PROMOTED</span>\n    <img class=\"polar-sponsorlogo\" src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Hulu_logo.svg/2000px-Hulu_logo.svg.png\" height=\"30px\" alt=\"Hulu\">\n    \n  </div>\n  </div>\n\n";  };
 
     }
 
