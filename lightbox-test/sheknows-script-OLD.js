45579d4e8ead466afc9b6b792e9721bdc291f8e1
Anna Ma <anna.ma@polar.me>
2016-10-05 15:34:37 -0400

    change the script to she knows script instead

--- lightbox-test/sheknows-script-OLD.js
+++ lightbox-test/sheknows-script-OLD.js
@@ -10,7 +10,7 @@
         return window.NATIVEADS_QUEUE;
     };
 
-    q().push(["setPropertyID", "NA-CONDNASTTRAV-11236271"]);
+    q().push(["setPropertyID", "NA-DANITEST-11237996"]);
     q().push(["setSecondaryPageURL", "/sample/publisher/sponsored.html"]);
 
 
@@ -19,9 +19,9 @@
         template: compiledTemplate0,
         unit: {
             "server": "mvdirect",
-            "id": "63664ae7e8bd463b9b6f2ee545f953bb"
+            "id": "367259feb2fd4d99963c609445b80135"
         },
-        location: "#panel > div > div > div:nth-child(1) > p:nth-child(7)",
+        location: "#panel > div > div > div:nth-child(1) > p:nth-child(8)",
 
         // iframeMode: true,
         // location: "body",
@@ -32,87 +32,95 @@
         // },
         onFill: function(data) {},
         onRender: function($element) {
-            jQuery('#sidebar-wrap > a').remove();
-            jQuery('#sidebar-wrap > div.rad.box.sidebar-rad.marg-b-med.sidebar-inview').remove();
         },
         onError: function(error) {}
     }]);
 
 
         q().push(["injectCSS", ["",
-"@font-face {",
-"    font-family:'Brandon Text';",
-"    src: url('https://static.polarcdn.com/vendor/fonts/BrandonText-Bold.otf') format('opentype');",
-"}",            
-".plr-promo-unit {",
-"    display: inline-block;",
-"    position: relative;",
-"}",
-"",
-".plr-promo-unit a {",
-"    color: initial;",
-"    text-decoration: initial;",
-"}",
+"@import 'https://fonts.googleapis.com/css?family=Roboto';",
 "",
 ".plr-rr {",
-"    position: relative;",
-"    width: 300px;",
-"    height: 250px;",
-"    background: #fff;",
-"    border-top: 3px solid #000;",
-"    display: inline-block;",
-"    overflow: hidden;",
-                                "margin:auto;",
+"  width: 300px;",
+"  height: 250px;",
+"  /*overflow: hidden;*/",
+"  position: relative;",
+"  margin: auto;",
+"  text-align: left;",
+"  border: 1px solid #c0c3c4;",
 "}",
 "",
-".plr-ad-unit {",
-"    position: absolute;",
-"    width: 274px;",
-"    left: 13px;",
-"    top: 13px;",
-"    bottom: 10px;",
-"    right: 13px;",
-"    margin: auto;",
+".plr-rr a {",
+"  color: initial;",
+"  text-decoration: initial;",
 "}",
 "",
-".plr-thumbnail {",
-"    position: relative;",
-"    width: 100%;",
-"    height: 150.013px;",
-"    background: #fff;",
+".plr-rr .plr-img-wrapper {",
+"  position: relative;",
+"  top: 0;",
+"  left: 0;",
+"  overflow: hidden;",
+"  width: 100%;",
+"  height: 100%;",
 "}",
 "",
-".plr-ad-content {",
-"    position: relative;",
-"    margin-top: 16px;",
-"    -webkit-font-smoothing: antiliased;",
+".plr-rr .plr-img-wrapper > div:first-child {",
+"  position: absolute;",
+"  top: 0;",
+"  right: 0;",
+"  bottom: 0;",
+"  left: 0;",
+"  transition: .25s;",
+"  background-size: cover !important;",
 "}",
 "",
-".plr-sponsored {",
-"    font-family: Brandon Text;",
-"    font-weight: bold;",
-"    text-transform: uppercase;",
-"    font-size: 10px;",
-"    line-height: 10px;",
-"    color: #f5823f;",
-"    letter-spacing: 0.08em;",
-"    -webkit-font-smoothing: antiliased;",
-"    -moz-font-smoothing: antiliased;",
+".plr-rr .plr-img-wrapper {",
+"  transition: .3s;",
 "}",
 "",
-".plr-title {",
-"    font-family: Brandon Text;",
-"    font-weight: bold;",
-"    font-size: 18px;",
-"    line-height: 20px;",
-"    margin-top: 4px;",
-"    color: #000;",
-"    -webkit-font-smoothing: antiliased;",
-"    -moz-font-smoothing: antiliased;",
+".plr-rr .plr-rr-logo {",
+"  position: relative;",
+"  float: right;",
+"  margin-right: 5px;",
+"  bottom: 35px;",
+"}",
+"",
+".plr-rr__content {",
+"  position: absolute;",
+"  bottom: 0;",
+"  left: 0;",
+"  width: 100%;",
+"  background-color: transparent;",
+"  font-family: 'Roboto', sans-serif;",
+"}",
+"",
+".plr-rr__content__title {",
+"  font-weight: normal;",
+"  font-size: 18px;",
+"  color: white;",
+"  padding: 10px;",
+"}",
+"",
+".plr-rr__content__banner {",
+"  font-family: 'Roboto', sans-serif;",
+"  display: inline-block;",
+"  font-size: 8px;",
+"  font-weight: bold;",
+"  line-height: 16px;",
+"  letter-spacing: 1.2px;",
+"  text-transform: uppercase;",
+"  position: absolute;",
+"  background-color: white;",
+"  border-radius: 3px;",
+"  padding: 3.2px 4.8px;",
+"  margin: 4px;",
+"  color: #666;",
+"  right: 0;",
+"  top: 0;",
 "}",
 ""].join("\n"), "head"]);
 
-          //lightbox
+        //lightbox
     q().push(["injectCSS", ["",
         "@import 'https://fonts.googleapis.com/css?family=Gruppo';",
         ".polar-deck-dialog {",
@@ -239,25 +247,23 @@
 
    Note that this code has been generated from the following markup:
 
-<div class="plr-promo-unit">
-  <a href="{{link}}">
-    <div class="plr-rr">
-      <div class="plr-ad-unit">
-              <div class="plr-thumbnail" style="background: url('{{getThumbHref}}') no-repeat center center;background-size:cover;">
-        </div>
-        <div class="plr-ad-content">
-          <div class="plr-sponsored">Sponsor Content</div>
-                  <div class="plr-title">{{title}}</div>
-        </div>
-      </div>
+<div class="plr-rr">
+  <a href="#polar-deck-link" rel="nofollow">
+    <div class="plr-img-wrapper">
+      <div style="background: url('{{getThumbHref}}') no-repeat center center;"></div>
     </div>
-
+    <div class="plr-rr__content__banner">Sponsored</div>
+    <div class="plr-rr__content">
+      
+      <div class="plr-rr__content__title">{{title}}</div>
+      
+    </div>
   </a>
 </div>
 
 */
 
-  compiledTemplate0 = function (Handlebars,depth0,helpers,partials,data) {  this.compilerInfo = [4,'>= 1.0.0'];helpers = this.merge(helpers, Handlebars.helpers); data = data || {};  var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression;  buffer += "<div class=\"plr-promo-unit\">\n  <a href=\"";  if (stack1 = helpers.link) { stack1 = stack1.call(depth0, {hash:{},data:data}); }  else { stack1 = depth0.link; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }  buffer += escapeExpression(stack1)    + "\">\n    <div class=\"plr-rr\">\n      <div class=\"plr-ad-unit\">\n              <div class=\"plr-thumbnail\" style=\"background: url('";  if (stack1 = helpers.getThumbHref) { stack1 = stack1.call(depth0, {hash:{},data:data}); }  else { stack1 = depth0.getThumbHref; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }  buffer += escapeExpression(stack1)    + "') no-repeat center center;background-size:cover;\">\n        </div>\n        <div class=\"plr-ad-content\">\n          <div class=\"plr-sponsored\">Sponsor Content</div>\n                  <div class=\"plr-title\">";  if (stack1 = helpers.title) { stack1 = stack1.call(depth0, {hash:{},data:data}); }  else { stack1 = depth0.title; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }  buffer += escapeExpression(stack1)    + "</div>\n        </div>\n      </div>\n    </div>\n\n  </a>\n</div>";  return buffer;  };
+  compiledTemplate0 = function (Handlebars,depth0,helpers,partials,data) {  this.compilerInfo = [4,'>= 1.0.0'];helpers = this.merge(helpers, Handlebars.helpers); data = data || {};  var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression;  buffer += "<div class=\"plr-rr\">\n  <a href=\"#polar-deck-link\" rel=\"nofollow\">\n    <div class=\"plr-img-wrapper\">\n      <div style=\"background: url('";  if (stack1 = helpers.getThumbHref) { stack1 = stack1.call(depth0, {hash:{},data:data}); }  else { stack1 = depth0.getThumbHref; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }  buffer += escapeExpression(stack1)    + "') no-repeat center center;\"></div>\n    </div>\n    <div class=\"plr-rr__content__banner\">Sponsored</div>\n    <div class=\"plr-rr__content\">\n      \n      <div class=\"plr-rr__content__title\">";  if (stack1 = helpers.title) { stack1 = stack1.call(depth0, {hash:{},data:data}); }  else { stack1 = depth0.title; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }  buffer += escapeExpression(stack1)    + "</div>\n      \n    </div>\n  </a>\n</div>";  return buffer;  };
 
     }
 
